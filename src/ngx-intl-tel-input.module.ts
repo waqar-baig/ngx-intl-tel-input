c4f2fb28b4920750cbe3872a940dd54375543c88
Waqar Baig <waqar.baig@ymail.com>
2018-11-26 15:34:25 +0530

    adding module file

---
+++ src/ngx-intl-tel-input.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgxIntlTelInputComponent } from './ngx-intl-tel-input.component';
+
+@NgModule({
+  declarations: [ NgxIntlTelInputComponent ],
+  imports: [ CommonModule ],
+  exports: [ NgxIpComponent ]
+})
+export class NgxIntlTelInputModule { }
